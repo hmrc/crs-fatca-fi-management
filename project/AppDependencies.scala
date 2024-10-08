f9ad2158ea2dd701bcbb210c7a5971c5c6e0a6bc
oarshad <omer.arshad@digital.hmrc.gov.uk>
2024-10-08 04:26:49 +0100

    DAC6-3330: Update dependency versions
    Upgraded `bootstrap-backend-play-30` to version 9.5.0 and `domain-play-30` to version 10.0.0. This change ensures compatibility with the latest updates and enhancements in each dependency.

--- project/AppDependencies.scala
+++ project/AppDependencies.scala
@@ -2,11 +2,12 @@ import sbt._
 
 object AppDependencies {
 
-  private val bootstrapVersion = "9.0.0"
+  private val bootstrapVersion = "9.5.0"
+  private val domainVersion = "10.0.0"
 
   val compile: Seq[ModuleID] = Seq(
     "uk.gov.hmrc"  %% "bootstrap-backend-play-30" % bootstrapVersion,
-    "uk.gov.hmrc"  %% "domain-play-30"            % bootstrapVersion,
+    "uk.gov.hmrc"  %% "domain-play-30"            % domainVersion,
     "com.beachape" %% "enumeratum-play"           % "1.8.0"
   )
 
