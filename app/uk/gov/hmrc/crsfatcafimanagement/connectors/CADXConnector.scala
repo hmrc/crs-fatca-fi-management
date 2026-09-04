ed9579e6b8cb4ab75f1039a0d4deb411e02baa30
jayandran-Sampath <24522469+jayandran-Sampath@users.noreply.github.com>
2026-09-04 14:40:41 +0100

    DAC6-4455 | use uuid instead of session value

--- app/uk/gov/hmrc/crsfatcafimanagement/connectors/CADXConnector.scala
+++ app/uk/gov/hmrc/crsfatcafimanagement/connectors/CADXConnector.scala
@@ -86,22 +86,17 @@ class CADXConnector @Inject() (
 
   private[connectors] def addHeaders(eisEnvironment: String)(implicit headerCarrier: HeaderCarrier): Seq[(String, String)] = {
     // HTTP-date format defined by RFC 7231 e.g. Fri, 01 Aug 2020 15:51:38 UTC
-    val formatter                      = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss 'UTC'").withZone(ZoneId.of("UTC"))
-    val stripSession: String => String = (input: String) => input.replace("session-", "")
+    val formatter = DateTimeFormatter.ofPattern("EEE, dd MMM yyyy HH:mm:ss 'UTC'").withZone(ZoneId.of("UTC"))
 
     Seq(
-      "x-forwarded-host" -> "mdtp",
-      "date"             -> ZonedDateTime.now().format(formatter),
-      "x-correlation-id" -> UUID.randomUUID().toString,
-      "x-conversation-id" -> headerCarrier.sessionId
-        .map(
-          id => stripSession(id.value)
-        )
-        .getOrElse(UUID.randomUUID().toString),
-      "x-regime-type" -> "CRSFATCA",
-      "content-type"  -> "application/json",
-      "accept"        -> "application/json",
-      "Environment"   -> eisEnvironment
+      "x-forwarded-host"  -> "mdtp",
+      "date"              -> ZonedDateTime.now().format(formatter),
+      "x-correlation-id"  -> UUID.randomUUID().toString,
+      "x-conversation-id" -> UUID.randomUUID().toString,
+      "x-regime-type"     -> "CRSFATCA",
+      "content-type"      -> "application/json",
+      "accept"            -> "application/json",
+      "Environment"       -> eisEnvironment
     )
   }
 
